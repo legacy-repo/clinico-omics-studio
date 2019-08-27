ca67815395819ed26b5c1b886dbe92598fb03fe4
YJC <yjcyxky@163.com>
2019-08-27 13:59:44 +0800

    First commit for datains frontend.

---
+++ src/components/GlobalFooter/GlobalFooter.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="footer">
+    <div class="links">
+      <a
+        href="https://www.3steps.cn"
+        target="_blank"
+      >智汇医圈联盟</a>
+      <a
+        href="https://github.com/go-choppy"
+        target="_blank"
+      >
+        <a-icon type="github" />
+      </a>
+      <a href="http://choppy.3steps.cn/">Choppy Platform</a>
+    </div>
+    <div class="copyright">
+      Copyright
+      <a-icon type="copyright" /> 2019 <span>The Genius Medicine Consortium</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GlobalFooter',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.footer {
+  padding: 0 16px;
+  margin: 10px 0;
+  text-align: center;
+
+  .links {
+    margin-bottom: 8px;
+
+    a {
+      color: rgba(0, 0, 0, 0.45);
+
+      &:hover {
+        color: rgba(0, 0, 0, 0.65);
+      }
+
+      &:not(:last-child) {
+        margin-right: 40px;
+      }
+    }
+  }
+  .copyright {
+    color: rgba(0, 0, 0, 0.45);
+    font-size: 14px;
+  }
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,30 @@
+// ie polyfill
+import '@babel/polyfill'
+
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store/'
+import { VueAxios } from './utils/request'
+
+// mock
+import './mock'
+
+import bootstrap from './core/bootstrap'
+import './core/use'
+import './permission' // permission control
+import './utils/filter' // global filter
+import i18n from './locales'
+
+Vue.config.productionTip = false
+
+// mount axios Vue.$http and this.$http
+Vue.use(VueAxios)
+
+new Vue({
+  router,
+  store,
+  i18n,
+  created: bootstrap,
+  render: h => h(App)
+}).$mount('#app')

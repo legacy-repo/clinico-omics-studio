9292765c1833bb795fe7080facd885229734298f
YJC <yjcyxky@163.com>
2019-12-09 23:39:08 +0800

    Improve the default config for theme.

--- src/config/defaultSettings.js
+++ src/config/defaultSettings.js
@@ -14,14 +14,14 @@
  */
 
 export default {
-  primaryColor: '#52C41A', // primary color of ant design
-  navTheme: 'dark', // theme for nav menu
+  primaryColor: '#2F54EB', // primary color of ant design
+  navTheme: 'light', // theme for nav menu
   layout: 'sidemenu', // nav menu position: sidemenu or topmenu
-  contentWidth: 'Fixed', // layout of content: Fluid or Fixed, only works when layout is topmenu
+  contentWidth: 'Fluid', // layout of content: Fluid or Fixed, only works when layout is topmenu
   hiddenHeader: false, // Hide header
-  fixedHeader: false, // sticky header
+  fixedHeader: true, // sticky header
   fixSiderbar: false, // sticky siderbar
-  autoHideHeader: false, //  auto hide header
+  autoHideHeader: true, //  auto hide header
   colorWeak: false,
   multiTab: false,
   production: process.env.NODE_ENV === 'production' && process.env.VUE_APP_PREVIEW !== 'true',
